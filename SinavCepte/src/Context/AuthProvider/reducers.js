4de07a35489f6b6c1bdaed88d8b9123ece8b1bbe
Zehra Tok <zehratok1@outlook.com>
2022-04-09 03:17:34 +0300

    Update reducers.js

--- SinavCepte/src/Context/AuthProvider/reducers.js
+++ SinavCepte/src/Context/AuthProvider/reducers.js
@@ -1,13 +1,15 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
+
 export default function reducers(state, action) {
     switch (action.type) {
         case 'SET_USER':
             const { data } = action.payload;
-            data ?
-                AsyncStorage.setItem("@USER", JSON.stringify(data)) :
-                AsyncStorage.removeItem("@USER");
+            AsyncStorage.setItem("@USER", JSON.stringify(data));
+            return { ...state, data };
 
-            return { ...state, data }
+        case 'REMOVE_USER':
+            AsyncStorage.removeItem("@USER");
+            return { ...state, data: null };
 
         default:
             return state;

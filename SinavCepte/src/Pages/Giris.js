477f1d266e0b4276a0e1aa9bfbbba4318f9c88b2
Zehra Tok <zehratok1@outlook.com>
2022-04-09 03:16:56 +0300

    Update Giris.js

--- SinavCepte/src/Pages/Giris.js
+++ SinavCepte/src/Pages/Giris.js
@@ -1,49 +1,32 @@
 import React, { useState, useEffect } from 'react';
-import {
-    Alert,
-    ScrollView,
-    Text,
-    TextInput,
-    TouchableOpacity,
-    View,
-} from 'react-native';
+import { Alert, ScrollView, TextInput, View, } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Resim from '../Components/Resim';
 import Link from '../Components/Links';
-import { ActivityIndicator } from 'react-native-paper';
 import usePost from '../Hooks/usePost';
 import { useDispatch } from 'react-redux';
 
 import styles from '../Styles/Giris.style';
 import Buton from '../Components/Buton';
+import { Formik } from 'formik';
 
 const Giris = () => {
     const { data, loading, error, post } = usePost();
     const dispatch = useDispatch();
-    const [mail, setMail] = useState('');
-    const [parola, setParola] = useState('');
 
-    const handleSubmit = async () => {
-        if (!mail && !parola) {
-            Alert.alert("HATA!", "Bilgiler boş bırakılamaz!");
-            return;
-        }
-        if (!mail) {
+    function handleGiris(values) {
+        if (!values.mail) {
             Alert.alert("HATA!", "E-posta adresi alanı boş bırakılamaz!");
             return;
         }
-        if (!parola) {
-            Alert.alert("HATA!", "Parola boş bırakılamaz!");
+        if (!values.parola) {
+            Alert.alert("HATA!", "Parola alanı boş bırakılamaz!");
             return;
         }
-        const kullanici = {
-            mail: mail,
-            parola: parola,
-        };
-        post("http://10.55.184.62:3001/giris-yap", kullanici);
-        console.log(data);
+        post("http://10.55.185.37:3001/giris-yap", values);
+
         if (error || data == null) {
-            Alert.alert("Üzgünüz", "Beklenmedik bir hata oluştu.");
+            Alert.alert("Beklenmedik bir hata oluştu.", "Tekrar deneyin.");
             return;
         }
         if (data == "Hatalı giriş") {
@@ -51,33 +34,44 @@ const Giris = () => {
             return;
         }
         else {
-            //Alert.alert("HOŞ GELDİNİZ!", "Giriş Başarılı.")
+            Alert.alert("HOŞ GELDİNİZ!", "Giriş Başarılı.")
             dispatch({ type: 'SET_USER', payload: { data } })
         }
     }
-    
+
     return (
         <ScrollView style={styles.container}>
             <SafeAreaView style={styles.giris}>
                 <Resim kaynak={require('../Resimler/giris.png')} />
-                <View>
-                    <View style={styles.form}>
-                        <TextInput
-                            style={styles.input}
-                            placeholder="E-posta adresi"
-                            onChangeText={setMail}
-                        />
-                        <TextInput
-                            style={styles.inputSon}
-                            placeholder="Parola"
-                            onChangeText={setParola}
-                        />
-                    </View >
-                    <View style={styles.parolaLink}>
-                        <Link to={{ screen: 'Parolanı Sıfırla' }} text='Parolanı mı unuttun?' />
-                    </View>
-                    <Buton text='GİRİŞ YAP' onPress={handleSubmit} />
-                </View >
+
+                <Formik initialValues={{ mail: '', parola: '' }}
+                    onSubmit={handleGiris}>
+                    {({ handleSubmit, handleChange, values }) => (
+                        <View>
+                            <View style={styles.form}>
+                                <TextInput
+                                    style={styles.input}
+                                    placeholder="E-posta adresi"
+                                    values={values.mail}
+                                    onChangeText={handleChange('mail')}
+                                />
+                                <TextInput
+                                    style={styles.inputSon}
+                                    placeholder="Parola"
+                                    values={values.parola}
+                                    onChangeText={handleChange('parola')}
+                                    secureTextEntry={true}
+                                />
+                            </View >
+                            <View style={styles.parolaLink}>
+                                <Link to={{ screen: 'Parolanı Sıfırla' }} text='Parolanı mı unuttun?' />
+                            </View>
+                            <Buton text='GİRİŞ YAP'
+                                onPress={handleSubmit}
+                                loading={loading} />
+                        </View >
+                    )}
+                </Formik>
                 <Link to={{ screen: 'Kaydol' }}
                     text=' Hesabın yok mu? Yeni bir hesap oluştur.' />
             </SafeAreaView >

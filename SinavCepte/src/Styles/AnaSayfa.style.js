bc9ee9a7c716813e9a777e3ae678a5356b4d7f57
Zehra Tok <zehratok1@outlook.com>
2022-04-09 03:17:41 +0300

    Create AnaSayfa.style.js

---
+++ SinavCepte/src/Styles/AnaSayfa.style.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native"
+
+export default StyleSheet.create({
+    container: {
+        display: 'flex',
+        flex: 1,
+    },
+    baslik: {
+        alignSelf: 'center',
+        fontSize: 40,
+        fontWeight: 'bold',
+        color: '#BE9FE1',
+    },
+    deneme: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+})

26c749ba0de8b8a3d94aeb0361bfa0b5094e0e74
Zehra Tok <zehratok1@outlook.com>
2022-05-07 23:40:09 +0300

    Create MotivasyonItems.js

---
+++ SinavCepte/src/Constants/MotivasyonItems.js
@@ -0,0 +1,32 @@
+export default [
+    {
+        id: 1,
+        soz: 'Hayatta kalabilen en güçlü olan tür değildir, en zeki olandır. Değişime en çok adapte olabilendir.',
+        soyleyen: 'Charles Robert Darwin'
+    },
+    {
+        id: 2,
+        soz: 'Hayat, bisiklete binmek gibidir. Dengede kalmak için hareket etmek zorundasınız.',
+        soyleyen: 'Albert Einstein'
+    },
+    {
+        id: 3,
+        soz: 'Başarısızlık da bir seçenektir. Eğer ki başarısız olmuyorsanız, yeterince girişimde bulunmuyorsunuz demektir.',
+        soyleyen: 'Elon Musk'
+    },
+    {
+        id: 4,
+        soz: 'Bazen, kimsenin hakkında hiçbir şey düşünmediği insanlar, kimsenin hakkında hiçbir şey düşünmediği şeyleri yapabilen kişilerdir.',
+        soyleyen: 'Alan Turing'
+    },
+    {
+        id: 5,
+        soz: 'İnsanlar konusunda daha az, fikirler konusunda daha çok meraklı olun.',
+        soyleyen: 'Marie Curie',
+    },
+    {
+        id: 6,
+        soz: 'Yaşam ne kadar kötü gözükürse gözüksün, her zaman yapabileceğiniz bir şeyler vardır. Mutlaka başarabileceğiniz bir şeyler vardır. Yaşamın olduğu yerde, umut da vardır.',
+        soyleyen: 'Stephen Hawking',
+    },
+]

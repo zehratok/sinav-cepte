d98e6e3cff91b8b6682591ef5802228e7f89fc9f
Zehra Tok <zehratok1@outlook.com>
2022-05-12 19:41:36 +0300

    Create Profile Screen

--- SinavCepte/src/Pages/Profil.js
+++ SinavCepte/src/Pages/Profil.js
@@ -1,8 +1,9 @@
+import { Ionicons, MaterialIcons } from '@expo/vector-icons'
 import React, { useState } from 'react'
-import { RefreshControl, ScrollView, Text } from 'react-native'
+import { Dimensions, Image, ImageBackground, RefreshControl, ScrollView, Text, View } from 'react-native'
+import { TouchableOpacity } from 'react-native-gesture-handler'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { useDispatch, useSelector } from 'react-redux'
-import Buton from '../Components/Buton'
 import DurumCubugu from '../Components/DurumCubugu'
 import Header from '../Components/Header'
 import styles from '../Styles/Profil.style'
@@ -27,15 +28,33 @@ const Profil = () => {
             }
         >
             <DurumCubugu />
-            <Header />
-            <SafeAreaView style={styles.deneme}>
-                <DurumCubugu />
-                <Text style={styles.baslik}>Merhaba {data.adSoyad} </Text>
+            <ImageBackground source={require('../Resimler/drawer.png')}
+                style={[styles.profil, { width: Dimensions.get('window').width, height: Dimensions.get('window').height }]}
+            >
+                <Header baslik="Profil" />
 
-                <Buton text="ÇIKIŞ YAP"
-                    onPress={() => dispatch({ type: 'REMOVE_USER' })} />
-            </SafeAreaView>
+                <SafeAreaView style={styles.profil}>
+                    {/* <Text style={styles.baslik}>Merhaba {data.adSoyad} </Text> */}
+                    <View style={{ alignSelf: "center" }}>
+                        <View style={styles.kutu}>
+
+                        </View>
+                        <View style={styles.profilResmi}>
+                            <Image source={require('../Resimler/profil.png')}
+                                style={styles.resim} />
+                        </View>
+                        <View style={styles.ekle}>
+                            <TouchableOpacity>
+                                <MaterialIcons name="add" size={30} color="white" style={{ fontWeight: "bold" }} />
+                            </TouchableOpacity>
+                        </View>
+
+                    </View>
+
+                </SafeAreaView>
+            </ImageBackground>
         </ScrollView >
+
     )
 }
 
--- SinavCepte/src/Styles/Profil.style.js
+++ SinavCepte/src/Styles/Profil.style.js
@@ -4,16 +4,49 @@ export default StyleSheet.create({
     container: {
         display: 'flex',
         flex: 1,
+        backgroundColor: '#BE9FE1',
     },
-    baslik: {
-        alignSelf: 'center',
-        fontSize: Dimensions.get('window').width/20,
-        fontWeight: 'bold',
-        color: '#BE9FE1',
-    },
-    deneme: {
+    profil: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
     },
+    kutu: {
+        height: Dimensions.get('window').height,
+        width: Dimensions.get('window').width,
+        paddingVertical: Dimensions.get('window').height / 3,
+        top: Dimensions.get('window').height / 5,
+        backgroundColor: 'white',
+        borderTopLeftRadius: 60,
+        borderTopRightRadius: 60,
+        bottom: 0,
+        left: -(Dimensions.get('window').width / 5),
+        borderRadius: 30,
+        position: 'absolute'
+    },
+    profilResmi: {
+        alignItems: 'center',
+        width: (Dimensions.get('window').height / 3) + 2,
+        height: (Dimensions.get('window').height / 3) + 2,
+        borderRadius: 100,
+        overflow: 'hidden',
+        top: Dimensions.get('window').height / 12,
+    },
+    resim: {
+        flex: 1,
+        padding: Dimensions.get('window').height / 9,
+        width: 100,
+        height: 100,
+        position: 'absolute'
+    },
+    ekle: {
+        backgroundColor: "#C9B6E4",
+        position: "absolute",
+        bottom: 35,
+        right: 50,
+        width: 40,
+        height: 40,
+        borderRadius: 30,
+        alignItems: "center",
+        justifyContent: "center"
+    }
+
 })

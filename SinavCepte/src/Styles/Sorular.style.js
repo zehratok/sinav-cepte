d4d6ec08afcd93c6d19146960ea7feda400078f3
Zehra Tok <zehratok1@outlook.com>
2022-06-11 01:42:02 +0300

    Create Sorular.style.js

---
+++ SinavCepte/src/Styles/Sorular.style.js
@@ -0,0 +1,12 @@
+import { Dimensions, StyleSheet } from "react-native";
+export default StyleSheet.create({
+    container: {
+        display: 'flex',
+        flex: 1,
+        justifyContent: 'space-between'
+    },
+    soru: {
+        flex: 1,
+
+    },
+})

d13be5441443796b6d818f2c3a00fb0aa1372660
Zehra Tok <zehratok1@outlook.com>
2022-06-12 22:37:32 +0300

    List unsolved questions on YKS screen

---
+++ SinavCepte/src/Components/HelperComponents/SoruKutu.js
@@ -0,0 +1,104 @@
+import { Image, ImageBackground, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React, { useEffect, useState } from 'react';
+import base64 from 'react-native-base64';
+import Font from '../Font';
+const SoruKutu = (props) => {
+    const resimb = props.resim;
+    const res = base64.encodeFromByteArray(resimb, Uint8Array);
+    const tarih = (props.tarih).slice(0, 10).replace('T', ' ')
+    const zaman = (props.tarih).slice(10, 16).replace('T', ' ')
+    const [ders, setDers] = useState('Belirtilmedi');
+    useEffect(() => {
+        if (props.ders == 1) {
+            setDers("Matematik")
+        }
+        else if (props.ders == 2) {
+            setDers("Fen Bilimleri")
+        }
+        else if (props.ders == 3) {
+            setDers("Sosyal")
+        }
+        else if (props.ders == 4) {
+            setDers("Türkçe")
+        }
+    }, [ders])
+
+
+    return (
+
+        <TouchableOpacity style={styles.container}
+            onPress={props.onPress}>
+            <Font />
+            <View>
+                <Image
+                    style={{
+                        width: 250,
+                        height: 250,
+                        alignSelf: 'center',
+                        justifyContent: 'center',
+                        marginTop: 20,
+                        borderRadius: 5
+                    }}
+                    source={{
+                        uri: `data:image/png;base64, ${res}`
+                    }} />
+            </View>
+            <View style={styles.baslik}>
+                <Text style={styles.baslik}>{props.baslik}</Text>
+            </View>
+            <View style={styles.footer}>
+                <View style={{ marginBottom: 10 }}>
+                    <Text style={styles.ders}> Ders:  {ders}</Text>
+                    <Text style={styles.ders}> Konu:  {props.konu}</Text>
+                </View>
+                <View style={{ marginBottom: 10 }} >
+                    <Text style={styles.tarih}>{zaman}</Text>
+                    <Text style={styles.tarih}>{tarih}</Text>
+                </View>
+            </View>
+        </TouchableOpacity>
+
+
+    )
+}
+
+export default SoruKutu
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginHorizontal: 20,
+        borderRadius: 10,
+        backgroundColor: 'white',
+        marginBottom:20
+    },
+    soru: {
+        borderRadius: 50
+    },
+    baslik: {
+        marginHorizontal: 10,
+        fontFamily: 'Ubuntu_700Bold',
+        color: '#7a6790',
+        fontSize: 15
+    },
+    footer: {
+        marginTop: 10,
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    ders: {
+        marginHorizontal: 10,
+        fontFamily: 'Ubuntu_400Regular',
+        color: '#9c83b8',
+        fontSize: 12,
+    },
+    tarih: {
+        marginHorizontal: 20,
+        fontFamily: 'Ubuntu_300Light',
+        color: '#9c83b8',
+        fontSize: 10,
+        alignSelf: 'flex-end',
+        marginBottom: 2,
+    },
+
+})
--- SinavCepte/src/Screens/HelperScreens/SoruYKS.js
+++ SinavCepte/src/Screens/HelperScreens/SoruYKS.js
@@ -1,12 +1,58 @@
 import { View, Text, FlatList, ImageBackground, TouchableOpacity } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import axios from 'axios';
+import SoruKutu from '../../Components/HelperComponents/SoruKutu';
 import styles from '../../Styles/Sorular.style';
 const SoruYKS = (props) => {
+  const [sorular, setSorular] = useState([]);
+  useEffect(() => {
+    axios.get(`http://192.168.43.215:3001/sorular/${2}`).then((response) => {
+      setSorular(response.data);
+
+    });
+  });
+  useEffect(() => {
+    return () => {
+        setSorular(null);
+        console.log("Bellek temizlendi.");
+    }
+}, []);
   return (
     <ImageBackground source={require('../../Resimler/drawer.png')}
       style={[styles.soru, { width: undefined, height: undefined }]} >
       <SafeAreaView>
+        <FlatList
+          data={sorular}
+          renderItem={({ item }) => <View key={item.id}>
+            <SoruKutu
+              id={item.kullanici_id}
+              ad={item.kullanici_adi}
+              baslik={item.baslik}
+              sinav={item.sinav}
+              aciklama={item.aciklama}
+              resim={item.resim.data}
+              ders={item.ders}
+              konu={item.konu}
+              tarih={item.tarih}
+              onPress={() => props.navigation.navigate('Soru Detayı',
+                {
+                  soruId: item.id,
+                  soruBaslik: item.baslik,
+                  soruAciklama: item.aciklama,
+                  soruResim: item.resim.data,
+                  soruDers: item.ders,
+                  soruKonu: item.konu,
+                  soruTarih: item.tarih,
+                  soranId: item.kullanici_id,
+                  soranAd: item.kullanici_adi,
+                })}
+            />
+          </View>
+          }
+          keyExtractor={item => item.id}
+          inverted contentContainerStyle={{ flexDirection: 'column-reverse', marginBottom: 10 }}
+        />
       </SafeAreaView>
     </ImageBackground>
   )

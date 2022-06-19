e528dd0c5b32beef0cd4da0cca243dd565bca5f7
Zehra Tok <zehratok1@outlook.com>
2022-06-19 20:23:11 +0300

    Edit settings screen
    Code to update user information

--- SinavCepte/src/Screens/Ayarlar.js
+++ SinavCepte/src/Screens/Ayarlar.js
@@ -1,11 +1,39 @@
-import { RefreshControl, Text } from 'react-native'
-import React, { useState } from 'react';
+import { Alert, Image, RefreshControl, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import React, { useEffect, useState } from 'react';
 import styles from '../Styles/Ayarlar.style';
 import DurumCubugu from '../Components/DurumCubugu';
 import { ScrollView } from 'react-native-gesture-handler';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Header from '../Components/Headers/Header';
+import { useSelector } from 'react-redux';
+import usePut from '../Hooks/usePut';
+import { Formik } from 'formik';
+import { FontAwesome5, MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
+import Buton from '../Components/Buton';
+import * as ImagePicker from 'expo-image-picker';
+
+
 const Ayarlar = () => {
+  const veri = useSelector(s => s.data);
+  const { data, loading, error, put } = usePut();
+  const [ad, setAd] = useState(veri.adSoyad);
+  const [mail, setMail] = useState(veri.email);
+  const [parola, setParola] = useState(veri.parola);
+  const [parolat, setParolat] = useState(veri.parola);
+  const id = veri.id;
+  const [image, setImage] = useState('');
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [3, 4],
+      quality: 1,
+    });
+    if (!result.cancelled) {
+      setImage(result.uri)
+      console.log(image);
+    }
+  }
   const [refresh, setRefresh] = useState(false);
   const pullMe = () => {
     setRefresh(true);
@@ -13,6 +41,36 @@ const Ayarlar = () => {
       setRefresh(false)
     }, 10000)
   }
+  function handleGuncelle(values) {
+    if (values.parola != values.parolaTekrar) {
+      Alert.alert("UYARI!", "Parolayı kontrol ediniz.");
+      return;
+    }
+    if (!values.mevcutParola) {
+      Alert.alert('UYARI!', 'Bilgilerinizi güncellemek için mevcut parolanızı giriniz!');
+      return;
+    }
+    if (values.mevcutParola != veri.parola){
+      Alert.alert("UYARI!", "Mevcut parolanızı kontrol ediniz.");
+      return;
+    }
+
+      put(`http://192.168.43.215:3001/kullanici-guncelle/${id}`, values);
+
+  }
+  useEffect(() => {
+    console.log(data);
+    if (error || data == null) {
+      return;
+    }
+    if (data.mesaj == "Çift kayıt hatası") {
+      Alert.alert("HATA!", "Bu e-posta adresi zaten mevcut.");
+      return;
+    }
+    if (data.mesaj == "Kayit işlemi başarılı") {
+      Alert.alert("Güncelleme Başarılı!", "Bilgileriniz başarıyla güncellendi!");
+    }
+  }, [data])
   return (
     <ScrollView style={styles.container}
       refreshControl={
@@ -22,10 +80,101 @@ const Ayarlar = () => {
         />
       }
     >
-      <SafeAreaView style={styles.deneme}>
-        <DurumCubugu />
-        <Header />
-        <Text style={styles.baslik}>Ayarlar  </Text>
+      <SafeAreaView style={styles.ayarlar}>
+        <View
+          style={{ width: undefined, height: 60, backgroundColor: '#be9fe1' }}
+        >
+          <Header baslik="Ayarlar" />
+        </View>
+        <View style={{ alignSelf: "center" }}>
+          <View style={styles.kutu}>
+            <Formik
+              initialValues={{ id: veri.id, adSoyad: ad, mail: mail, mevcutParola: '', parola: parola, parolaTekrar: parolat }}
+              onSubmit={handleGuncelle}
+              enableReinitialize={true}
+            >
+              {({ handleSubmit, handleChange, values }) => (
+                <View style={styles.formContainer}>
+                  <View style={styles.form}>
+                    <View style={styles.inputGrup} >
+                      <FontAwesome5 name="user-alt" size={22} style={styles.icon} />
+                      <TextInput
+                        style={styles.input}
+                        placeholder={ad}
+                        defaultValue={veri.adSoyad}
+                        value={ad}
+                        onChangeText={setAd}
+                      />
+                    </View>
+                    <View style={styles.inputGrup} >
+                      <MaterialIcons name="email" size={22} style={styles.icon} />
+                      <TextInput
+                        style={styles.input}
+                        placeholder={mail}
+                        defaultValue={veri.email}
+                        value={mail}
+                        onChangeText={setMail}
+                      />
+                    </View>
+                    <View style={styles.inputGrup} >
+                      <MaterialCommunityIcons name="account-key" size={24} style={styles.icon} />
+                      <TextInput
+                        style={styles.input}
+                        placeholder="Mevcut parola"
+                        value={values.mevcutParola}
+                        onChangeText={handleChange('mevcutParola')}
+                        secureTextEntry={true}
+                      />
+                    </View>
+                    <View style={styles.inputGrup} >
+                      <MaterialIcons name="vpn-key" size={22} style={styles.icon} />
+                      <TextInput
+                        style={styles.input}
+                        placeholder="Parola"
+                        value={parola}
+                        onChangeText={setParola}
+                        secureTextEntry={true}
+                      />
+                    </View>
+                    <View style={styles.inputGrup} >
+                      <MaterialIcons name="vpn-key" size={22} style={styles.icon} />
+                      <TextInput
+                        style={styles.input}
+                        placeholder={parolat}
+                        value={parolat}
+                        onChangeText={setParolat}
+                        secureTextEntry={true}
+                      />
+                    </View>
+
+                  </View>
+                  <View style={styles.buton}>
+                    <TouchableOpacity onPress={handleSubmit} loading={loading}>
+                      <Text style={styles.butonText}>Bilgilerimi Güncelle</Text>
+                    </TouchableOpacity>
+                  </View>
+                </View>
+              )}
+            </Formik>
+          </View>
+          <View style={styles.profilResmi}>
+            <Image source={require('../Resimler/profil.png')}
+              style={styles.resim} />
+          </View>
+          <View style={styles.ekle}>
+            <TouchableOpacity>
+              <MaterialIcons name="add" size={30} color="white" style={{ fontWeight: "bold" }} />
+            </TouchableOpacity>
+          </View>
+          <View style={styles.nokta1}></View>
+          <View style={styles.nokta2}></View>
+          <View style={styles.nokta3}></View>
+          <View style={styles.nokta4}></View>
+          <View style={styles.nokta5}></View>
+          <View style={styles.nokta6}></View>
+
+
+        </View>
       </SafeAreaView>
     </ScrollView>
   )
--- SinavCepte/src/Styles/Ayarlar.style.js
+++ SinavCepte/src/Styles/Ayarlar.style.js
@@ -1,19 +1,149 @@
-import { Dimensions, StyleSheet } from "react-native";
+import { Dimensions, StyleSheet } from "react-native"
+
 export default StyleSheet.create({
     container: {
         display: 'flex',
         flex: 1,
-        backgroundColor: 'white'
+        backgroundColor: '#e8e4ec',
+
     },
-    deneme: {
+    ayarlar: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
     },
-    baslik: {
+    kutu: {
         alignSelf: 'center',
-        fontSize: Dimensions.get('window').width / 20,
-        fontWeight: 'bold',
-        color: '#BE9FE1',
+        height: 500,
+        width: Dimensions.get('window').width,
+        top: Dimensions.get('window').height / 5,
+        bottom: 0,
     },
+    formContainer: {
+        alignSelf: 'center',
+        bottom: 80,
+    },
+    form: {
+        alignSelf: 'center',
+        bottom: 80,
+    },
+    inputGrup: {
+        alignSelf: 'center',
+        flexDirection: 'row',
+        left: 30,
+        top: 250,
+    },
+    icon: {
+        alignSelf: 'center',
+        padding: 12,
+        color: '#7a6790'
+    },
+    input: {
+        alignSelf: 'center',
+        height: 45,
+        width: 275,
+        padding: 10,
+        fontSize: 17,
+        fontFamily: 'Ubuntu_400Regular',
+        color: '#9c83b8',
+    },
+    profilResmi: {
+        alignSelf: 'center',
+        width: (Dimensions.get('window').height / 3) + 2,
+        height: (Dimensions.get('window').height / 3) + 2,
+        borderRadius: 100,
+        overflow: 'hidden',
+        top: -470,
+        left: 40
+    },
+    resim: {
+        flex: 1,
+        padding: Dimensions.get('window').height / 9,
+        width: 100,
+        height: 100,
+        position: 'absolute'
+    },
+    ekle: {
+        backgroundColor: "#7a6790",
+        position: "absolute",
+        bottom: 565,
+        right: 135,
+        width: 40,
+        height: 40,
+        borderRadius: 30,
+        alignItems: "center",
+        justifyContent: "center"
+    },
+
+    buton: {
+        backgroundColor: '#7a6790',
+        alignItems: 'center',
+        borderRadius: 10,
+        top: 240,
+        marginBottom: Dimensions.get('window').height / 50,
+        height: Dimensions.get('window').height / 14,
+        padding: Dimensions.get('window').height / 50,
+    },
+    butonText: {
+        alignSelf: 'center',
+        color: '#e8e4ec',
+        fontSize: 18,
+        fontFamily: 'Ubuntu_700Bold',
+    },
+    nokta1: {
+        backgroundColor: "#9c83b8",
+        position: "absolute",
+        bottom: 620,
+        right: 258,
+        width: 6,
+        height: 6,
+        borderRadius: 30,
+
+    },
+    nokta2: {
+        backgroundColor: "#9c83b8",
+        position: "absolute",
+        bottom: 580,
+        right: 245,
+        width: 10,
+        height: 10,
+        borderRadius: 30,
+    },
+    nokta3: {
+        backgroundColor: "#dfcef0",
+        position: "absolute",
+        bottom: 650,
+        right: 120,
+        width: 5,
+        height: 5,
+        borderRadius: 30,
+
+    },
+    nokta4: {
+        backgroundColor: "#d4beeb",
+        position: "absolute",
+        bottom: 590,
+        right: 260,
+        width: 15,
+        height: 15,
+        borderRadius: 30,
+    },
+    nokta5: {
+        backgroundColor: "#9c83b8",
+        position: "absolute",
+        bottom: 630,
+        right: 110,
+        width: 15,
+        height: 15,
+        borderRadius: 30,
+    },
+    nokta6: {
+        backgroundColor: "#d4beeb",
+        position: "absolute",
+        bottom: 600,
+        right: 120,
+        width: 10,
+        height: 10,
+        borderRadius: 30,
+    },
+
+
 })
